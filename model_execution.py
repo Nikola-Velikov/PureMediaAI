b3f232a4b7a8093645e7ec9908a49a6643ff207a
Nikola Velikov <niki.velikov09@gmail.com>
2025-03-26 13:57:00 +0200

    Add install requirements

--- model_execution.py
+++ model_execution.py
@@ -1,9 +1,8 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Query
 import torch
 import torch.nn.functional as F
 from starlette.middleware.cors import CORSMiddleware
 from transformers import AutoTokenizer, AutoModelForSequenceClassification
-from pydantic import BaseModel
 import gc  # Garbage Collection
 
 app = FastAPI()
@@ -23,9 +22,6 @@ device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
 class_weights = torch.tensor([38.0, 1.0, 95.0], dtype=torch.float).to(device)
 
-class InputText(BaseModel):
-    text: str
-
 def load_model():
     global model, tokenizer
     if model is None or tokenizer is None:
@@ -34,11 +30,9 @@ def load_model():
         model = AutoModelForSequenceClassification.from_pretrained(model_name).to(device)
         model.eval()
 
-@app.post("/classify")
-def classify_text(input_data: InputText):
+@app.get("/classify")
+def classify_text(text: str = Query(..., description="Text to classify")):
     load_model()  # Load model only when needed
-
-    text = input_data.text
 
     # Tokenize input text (reduced max_length for efficiency)
     inputs = tokenizer(text, return_tensors="pt", truncation=True, padding=True, max_length=256).to(device)
@@ -66,3 +60,4 @@ def classify_text(input_data: InputText):
     label_map = {0: "Negative", 1: "Neutral", 2: "Positive"}
 
     return {"prediction": label_map[predicted_label], "confidence": round(confidence, 2)}
+
